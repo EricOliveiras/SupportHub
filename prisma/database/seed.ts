a43fdb9589e187a13757ca48ad1683f4816aabc2
Eric Nascimento <oiericoi@hotmail.com>
2024-09-14 20:28:47 -0300

    feat: add seed

---
+++ prisma/database/seed.ts
@@ -0,0 +1,57 @@
+import { PrismaClient } from '@prisma/client';
+import { saltRounds } from '../../src/config/vars';
+import { permission_role, permissions, roles } from './data';
+import {hash} from "bcryptjs";
+
+const prisma = new PrismaClient();
+
+const main = async () => {
+    console.log('> Seeding database...');
+
+    try {
+        await prisma.role.createMany({
+            data: roles,
+            skipDuplicates: true,
+        });
+
+        await prisma.permission.createMany({
+            data: permissions,
+            skipDuplicates: true,
+        });
+
+        await prisma.permission_Role.createMany({
+            data: permission_role,
+            skipDuplicates: true,
+        });
+
+        const admin = await prisma.user.upsert({
+            where: {
+                email: 'admin@example.com'
+            },
+            update: {},
+            create: {
+                fullName: 'admin',
+                email: 'admin@example.com',
+                password: await hash('admin', saltRounds),
+                roleId: 1
+            },
+        });
+
+        await prisma.user_Role.createMany({
+            data: {
+                role_id: admin.roleId,
+                user_id: admin.id
+            },
+            skipDuplicates: true
+        });
+
+    } catch (error) {
+        console.error(error);
+        await prisma.$disconnect();
+        process.exit(1);
+    } finally {
+        await prisma.$disconnect();
+    }
+};
+
+main();
