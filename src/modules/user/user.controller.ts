5cc5a9ff720e13afaced1436cf04d85ea6fd0e07
Eric Nascimento <oiericoi@hotmail.com>
2024-10-12 16:30:12 -0300

    refactor: modify user controller

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,6 +1,6 @@
 import {Request, Response} from 'express';
 import {UserService} from "./user.service";
-import {CreateUser, UserResponse} from "./user.interface";
+import {CreateUserDTO, UserResponseDTO} from "./user.interface";
 
 export class UsersController {
     private userService: UserService;
@@ -10,17 +10,17 @@ export class UsersController {
     }
 
     public async create(req: Request, res: Response): Promise<Response> {
-        const {fullName, email, password, roleId}: CreateUser = req.body;
+        const {fullName, email, password, roleId, sectorId}: CreateUserDTO = req.body;
 
-        const createUser: UserResponse = await this.userService.create({
+        const createUser: UserResponseDTO = await this.userService.create({
             fullName,
             email,
             password,
             roleId,
+            sectorId
         });
 
         return res.status(201).json({
-            error: false,
             message: 'User successfully created',
             user: createUser
         });
@@ -29,8 +29,25 @@ export class UsersController {
     public async findAll(req: Request, res: Response): Promise<Response> {
         const findUsers = await this.userService.findAll();
         return res.status(200).json({
-            error: false,
             users: findUsers
+        });
+    }
+
+    public async findById(req: Request, res: Response): Promise<Response> {
+        const {id} = req.params;
+        const user = await this.userService.findById(parseInt(id));
+        return res.status(200).json({
+            user: user
+        });
+    }
+
+    public async update(req: Request, res: Response): Promise<Response> {
+        const {id} = req.params;
+        const {fullName, roleId, sectorId} = req.body;
+        const user = await this.userService.update(parseInt(id), {fullName, roleId, sectorId});
+        return res.status(200).json({
+            message: 'User successfully updated',
+            user: user
         });
     }
 }
