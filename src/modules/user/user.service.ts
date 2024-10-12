c00284948773016c31578b575c24f5b8fc36a24b
Eric Nascimento <oiericoi@hotmail.com>
2024-10-12 16:30:02 -0300

    refactor: modify user service

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,16 +1,21 @@
-import {CreateUser, UserRepository, UserResponse} from "./user.interface";
+import {
+    CreateUserDTO,
+    IUserService, UpdateUserDTO, UserResponseDTO, UserWithoutPasswordDTO,
+} from "./user.interface";
 import {HttpException} from "../../errors/http.exception";
 import {saltRounds} from "../../config/vars";
 import {hash} from "bcryptjs";
+import {UserRepository} from "./user.repository";
+import {prisma} from "../../config/prisma";
 
-export class UserService {
+export class UserService implements IUserService {
     private userRepository: UserRepository;
 
     constructor(userRepository: UserRepository) {
         this.userRepository = userRepository;
     }
 
-    public async create(user: CreateUser): Promise<UserResponse> {
+    public async create(user: CreateUserDTO): Promise<UserResponseDTO> {
         const checkIfUserAlreadyExist = await this.userRepository.findByEmail(user.email);
 
         if (checkIfUserAlreadyExist) {
@@ -20,10 +25,11 @@ export class UserService {
         const hashPassword = await hash(user.password, saltRounds);
 
         const createdUser = await this.userRepository.create({
-            fullName: user.fullName,
+            fullName: user.fullName.toLowerCase(),
             email: user.email,
             password: hashPassword,
             roleId: user.roleId,
+            sectorId: user.sectorId,
         });
 
         await this.userRepository.addRole(createdUser.id, user.roleId);
@@ -31,16 +37,19 @@ export class UserService {
         return createdUser;
     }
 
-    public async findAll(): Promise<UserResponse[]> {
-        const findUsers = await this.userRepository.findAll();
+    public async findAll(): Promise<UserWithoutPasswordDTO[]> {
+        const users = await this.userRepository.findAll();
 
-        return findUsers.map(user => {
+        const usersWithoutPassword = users.map(user => {
             const {password, ...userWithoutPassword} = user;
             return userWithoutPassword;
         });
+
+        return usersWithoutPassword as UserWithoutPasswordDTO[];
     }
 
-    public async findById(id: number): Promise<UserResponse | null> {
+
+    public async findById(id: number): Promise<UserResponseDTO | null> {
         const findUser = await this.userRepository.findById(id);
 
         if (!findUser) {
@@ -48,5 +57,19 @@ export class UserService {
         }
 
         return findUser;
+    }
+
+    public async update(id: number, {fullName, sectorId, roleId}: UpdateUserDTO): Promise<UserResponseDTO> {
+        const findUser = await this.userRepository.findById(id);
+
+        if (!findUser) {
+            throw new HttpException(404, "user not found");
+        }
+
+        return await this.userRepository.update(id, {
+            fullName: fullName ? fullName.toLowerCase() : findUser.fullName,
+            sectorId: sectorId,
+            roleId: roleId,
+        });
     }
 }
