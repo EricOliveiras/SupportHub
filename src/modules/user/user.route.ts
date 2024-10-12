f44d6d4be770a88b38442625bfc44fca78b81d49
Eric Nascimento <oiericoi@hotmail.com>
2024-10-12 16:30:30 -0300

    refactor: modify user route

--- src/modules/user/user.route.ts
+++ src/modules/user/user.route.ts
@@ -1,16 +1,15 @@
 import {Router} from "express";
-import {UserRepositoryPrisma} from "./user.repository";
+import {UserRepository} from "./user.repository";
 import {UserService} from "./user.service";
 import {UsersController} from "./user.controller";
 
 export const userRouter = Router();
 
-const userRepository = new UserRepositoryPrisma();
+const userRepository = new UserRepository();
 const userService = new UserService(userRepository);
 const userController = new UsersController(userService);
 
 userRouter.post("/users", userController.create.bind(userController));
 userRouter.get("/users", userController.findAll.bind(userController));
-userRouter.get("/", (req, res)=> {
-    res.send("Hello World!");
-})
+userRouter.get("/users/:id", userController.findById.bind(userController));
+userRouter.put("/users/update/:id", userController.update.bind(userController))
