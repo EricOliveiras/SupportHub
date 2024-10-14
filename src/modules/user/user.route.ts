e27e1bfb41aa7f891a49c2284676526c6de82b13
Eric Nascimento <oiericoi@hotmail.com>
2024-10-14 13:16:01 -0300

    refactor: update user route

--- src/modules/user/user.route.ts
+++ src/modules/user/user.route.ts
@@ -2,6 +2,8 @@ import {Router} from "express";
 import {UserRepository} from "./user.repository";
 import {UserService} from "./user.service";
 import {UsersController} from "./user.controller";
+import {canRequest} from "../../middlewares/permissionsMiddleware";
+import {authenticated} from "../../middlewares/authMiddlware";
 
 export const userRouter = Router();
 
@@ -9,7 +11,28 @@ const userRepository = new UserRepository();
 const userService = new UserService(userRepository);
 const userController = new UsersController(userService);
 
-userRouter.post("/users", userController.create.bind(userController));
-userRouter.get("/users", userController.findAll.bind(userController));
-userRouter.get("/users/:id", userController.findById.bind(userController));
-userRouter.put("/users/update/:id", userController.update.bind(userController))
+userRouter.post("/users",
+    authenticated,
+    canRequest("create:user"),
+    userController.create.bind(userController)
+);
+userRouter.get("/users",
+    authenticated,
+    canRequest("read:user:list"),
+    userController.findAll.bind(userController)
+);
+userRouter.get("/users/:id",
+    authenticated,
+    canRequest("read:user"),
+    userController.findById.bind(userController)
+);
+userRouter.get("/users/profile/me",
+    authenticated,
+    canRequest("read:me"),
+    userController.me.bind(userController)
+);
+userRouter.put("/users/update/:id",
+    authenticated,
+    canRequest("update:user"),
+    userController.update.bind(userController)
+)
