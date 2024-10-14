60422f5f9d86c05e80d233c8b7a9609d93fc8b8b
Eric Nascimento <oiericoi@hotmail.com>
2024-10-14 13:14:08 -0300

    refactor: add authroute in server

--- src/config/server.ts
+++ src/config/server.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import {errorMiddleware} from "../middlewares/errorMiddleware";
 import {userRouter} from "../modules/user/user.route";
+import {authRouter} from "../modules/auth/authenticate.route";
 
 export const app = express();
 
 app.use(express.json());
 
+app.use(authRouter);
 app.use(userRouter);
 
 app.use(errorMiddleware);
