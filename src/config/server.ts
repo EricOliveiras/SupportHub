01542d70b1134de569596a900eab54e24f16f987
Eric Nascimento <oiericoi@hotmail.com>
2024-10-16 18:31:45 -0300

    refactor: add sector route in server.ts

--- src/config/server.ts
+++ src/config/server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import {errorMiddleware} from "../middlewares/errorMiddleware";
 import {userRouter} from "../modules/user/user.route";
 import {authRouter} from "../modules/auth/authenticate.route";
+import {sectorRouter} from "../modules/sector/sector.route";
 
 export const app = express();
 
@@ -9,5 +10,6 @@ app.use(express.json());
 
 app.use(authRouter);
 app.use(userRouter);
+app.use(sectorRouter);
 
 app.use(errorMiddleware);
