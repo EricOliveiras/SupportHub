0fb86333212d1f916cceeaf153eeeed3c1b5f9b1
Eric Nascimento <oiericoi@hotmail.com>
2024-10-16 18:29:45 -0300

    feat: add sector repository

--- src/modules/sector/sector.repository.ts
+++ src/modules/sector/sector.repository.ts
@@ -1,7 +1,16 @@
 import {prisma as db} from "../../config/prisma";
 import {Sector} from "@prisma/client";
+import {CreateSectorDTO, UpdateSectorDTO} from "./sector.interface";
 
 export class SectorRepository {
+    public async create(sector: CreateSectorDTO): Promise<Sector> {
+        return db.sector.create({
+            data: {
+                name: sector.name
+            }
+        });
+    }
+
     public async findAll(): Promise<Sector[]> {
         return db.sector.findMany({
             include: {
@@ -17,6 +26,31 @@ export class SectorRepository {
             include: {
                 users: true,
                 orders: true,
+            }
+        });
+    }
+
+    public async findByName(name: string): Promise<Sector | null> {
+        return db.sector.findUnique({
+            where: {
+                name: name
+            },
+        });
+    }
+
+    public async update(id: number, sector: UpdateSectorDTO): Promise<Sector> {
+        return db.sector.update({
+            where: {
+                id: id
+            },
+            data: sector
+        })
+    }
+
+    public async delete(id: number): Promise<void> {
+        await db.sector.delete({
+            where: {
+                id: id
             }
         });
     }
