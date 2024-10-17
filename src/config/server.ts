c0583bd4e33be1b6b6e173636868e8339d678d50
Eric Nascimento <oiericoi@hotmail.com>
2024-10-17 20:49:38 -0300

    feat: add ticket route in serve

--- src/config/server.ts
+++ src/config/server.ts
@@ -3,6 +3,7 @@ import {errorMiddleware} from "../middlewares/errorMiddleware";
 import {userRouter} from "../modules/user/user.route";
 import {authRouter} from "../modules/auth/authenticate.route";
 import {sectorRouter} from "../modules/sector/sector.route";
+import {ticketRouter} from "../modules/ticket/ticket.route";
 
 export const app = express();
 
@@ -11,5 +12,6 @@ app.use(express.json());
 app.use(authRouter);
 app.use(userRouter);
 app.use(sectorRouter);
+app.use(ticketRouter);
 
 app.use(errorMiddleware);
