29a8e249980295d843df17b2752ce8c943b46e33
Eric Nascimento <oiericoi@hotmail.com>
2024-10-19 10:55:37 -0300

    feat: add swagger configuration in server

--- src/config/server.ts
+++ src/config/server.ts
@@ -1,13 +1,19 @@
 import express from 'express';
+import cors from 'cors';
 import {errorMiddleware} from "../middlewares/errorMiddleware";
 import {userRouter} from "../modules/user/user.route";
 import {authRouter} from "../modules/auth/authenticate.route";
 import {sectorRouter} from "../modules/sector/sector.route";
 import {ticketRouter} from "../modules/ticket/ticket.route";
+import {setupSwagger} from "./swagger/swagger";
 
 export const app = express();
 
 app.use(express.json());
+
+app.use(cors());
+
+setupSwagger(app);
 
 app.use(authRouter);
 app.use(userRouter);
