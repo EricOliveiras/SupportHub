47ec7e224347df58b07a434341e6ec5bad6e5021
Eric Nascimento <oiericoi@hotmail.com>
2024-10-26 10:24:20 -0300

    refactor: modify server

--- src/config/server.ts
+++ src/config/server.ts
@@ -1,21 +1,24 @@
-import express from 'express';
-import cors from 'cors';
-import {errorMiddleware} from "../middlewares/errorMiddleware";
-import {userRouter} from "../modules/user/user.route";
-import {authRouter} from "../modules/auth/authenticate.route";
-import {sectorRouter} from "../modules/sector/sector.route";
-import {ticketRouter} from "../modules/ticket/ticket.route";
-import {setupSwagger} from "./swagger/swagger";
+import express from "express";
+import cors from "cors";
+import { errorMiddleware } from "../middlewares/errorMiddleware";
+import { userRouter } from "../modules/user/user.route";
+import { authRouter } from "../modules/auth/authenticate.route";
+import { sectorRouter } from "../modules/sector/sector.route";
+import { ticketRouter } from "../modules/ticket/ticket.route";
+import { setupSwagger } from "./swagger/swagger";
 
 export const app = express();
 
 app.use(express.json());
 
-app.use(cors({
-    origin: ["*"],
+app.use(
+  cors({
+    origin: "http://localhost:5173",
     methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ['Content-Type', 'Authorization']
-}));
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
 
 setupSwagger(app);
 
